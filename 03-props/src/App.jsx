753fd8fc0b20d8ddc0026125576894d87ba3fbb6
flexxycodes <tanzimrani8@gmail.com>
2026-01-01 16:39:55 +0530

    update App.jsx

--- 03-props/src/App.jsx
+++ 03-props/src/App.jsx
@@ -7,6 +7,25 @@ const App = () => {
       <div className="card">
         <div className="top">
           <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTMnYPXIDmRTKpj1drsmIRD_0NJJLVIVnMJNA&usqp=CAU"></img>
+          <button>Save</button>
+        </div>
+        <div className="center">
+          <h3>Amazon</h3> <span>5 days ago</span>
+          <h2>Senior UI/UX Designer</h2>
+          <div>
+            <h4>Part-Time</h4>
+            <h4>Senior Level</h4>
+          </div>
+        </div>
+        <div className="bottom">
+          <div>
+            <div>
+              <h3>120$/hr</h3>
+              <p>San-Fransisco</p>
+            </div>
+            <button>Apply Now
+            </button>
+          </div>
         </div>
       </div>
     </div>
