7b9f9cd78a1c5366eb4e3e85825982290b15cfba
flexxycodes <flexxycodes@gmail.com>
2026-01-02 20:13:20 +0530

    Update Navbar Styling

--- 06-tailwind/src/components/Section1/Navbar.jsx
+++ 06-tailwind/src/components/Section1/Navbar.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 const Navbar = () => {
   return(
     <div className='flex items-center justify-between px-12 py-6'>
-      <h4 className='bg-black text-white px-5 py-3 rounded-full'>TARGET AUDIENCE</h4>
-      <button> DIGITAL BANKING PLATFORM
+      <h4 className='bg-black text-white px-6 py-2 rounded-full'>TARGET AUDIENCE</h4>
+      <button className='bg-gray-100 px6 py-2 rounded-full tracking-widest text-xs'> DIGITAL BANKING PLATFORM
       </button>
     </div>
   )
