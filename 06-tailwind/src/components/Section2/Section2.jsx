bf36f73e650731dfe5b14cc918e44a28a59b372f
flexxycodes <flexxycodes@gmail.com>
2026-01-02 19:33:12 +0530

    update Section2.jsx

--- 06-tailwind/src/components/Section2/Section2.jsx
+++ 06-tailwind/src/components/Section2/Section2.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
 
-// Usage
 const Section2 = () => {
   return(
-  <div className='h-screen w-full bg-green-100'>
-    <h1>Section 2</h1>
+  <div className='h-screen w-full'>
+    <h1 className='color-black-200'>Section 2</h1>
   </div>
   )
 };
 
-export default Section1;
+export default Section2;
