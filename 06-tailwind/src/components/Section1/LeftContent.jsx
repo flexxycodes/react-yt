7c597760f79039352e8ac27cf4a8d2382dba4498
flexxycodes <flexxycodes@gmail.com>
2026-01-02 21:47:14 +0530

    update LeftContent.jsx

--- 06-tailwind/src/components/Section1/LeftContent.jsx
+++ 06-tailwind/src/components/Section1/LeftContent.jsx
@@ -4,7 +4,7 @@ import { ArrowUpRight } from 'lucide-react';
 
 const LeftContent = () => {
   return(
-    <div className='h-full flex flex-col justify-between w-1/3 px-4'>
+    <div className='h-full flex flex-col justify-between w-1/3 px-4 bg-black-50'>
       <div>
         <h3 className='mb-5 text-6xl font-bold'> Prospective <br/><span>Customer</span> <br/> Segmentation</h3>
         <p className='text-xl'>
