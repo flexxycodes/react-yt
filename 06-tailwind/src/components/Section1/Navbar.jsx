39a2f534c4800ad77995d6ea9c9ca35b800f32bf
flexxycodes <flexxycodes@gmail.com>
2026-01-02 20:02:10 +0530

    Fix Styling

--- 06-tailwind/src/components/Section1/Navbar.jsx
+++ 06-tailwind/src/components/Section1/Navbar.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Navbar = () => {
   return(
-    <div className='flex items-center justify-between px-10 py-5'>
-      <h4 className='bg-black text-white px-5 py-3 rounded-f'>TARGET AUDIENCE</h4>
+    <div className='flex items-center justify-between px-12 py-6'>
+      <h4 className='bg-black text-white px-5 py-3 rounded-full'>TARGET AUDIENCE</h4>
       <button> DIGITAL BANKING PLATFORM
       </button>
     </div>
