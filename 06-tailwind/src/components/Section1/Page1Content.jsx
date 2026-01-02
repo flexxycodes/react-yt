bab9680fb10443fdb849beb582243e4dd53c177e
flexxycodes <flexxycodes@gmail.com>
2026-01-02 20:22:49 +0530

    update

--- 06-tailwind/src/components/Section1/Page1Content.jsx
+++ 06-tailwind/src/components/Section1/Page1Content.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Page1Content = () => {
   return(
-    <div className='py-3 bg-blue-500 px-18'>
+    <div className='py-3 h-[90vh] bg-blue-500 px-18'>
       
     </div>
   )
