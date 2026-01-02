fbeeffb4f2fbdf624085f5651a2ac418045763c0
flexxycodes <flexxycodes@gmail.com>
2026-01-02 19:39:28 +0530

    update App.jsx

--- 06-tailwind/src/App.jsx
+++ 06-tailwind/src/App.jsx
@@ -4,11 +4,12 @@ import Section2 from './components/Section2/Section2'
 
 
 const App = () => {
-  return <div>
-    hi
+  return (
+  <div>
     <Section1 />
     <Section2 />
   </div>
+  )
 };
 
 export default App;
