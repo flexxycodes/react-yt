b5e299190ba8296a61e388a3e58ad702b7001c30
flexxycodes <flexxycodes@gmail.com>
2026-01-02 20:14:45 +0530

    update Navbar.jsx

--- 06-tailwind/src/components/Section1/Navbar.jsx
+++ 06-tailwind/src/components/Section1/Navbar.jsx
@@ -4,7 +4,7 @@ const Navbar = () => {
   return(
     <div className='flex items-center justify-between px-12 py-6'>
       <h4 className='bg-black text-white px-6 py-2 rounded-full'>TARGET AUDIENCE</h4>
-      <button className='bg-gray-100 px6 py-2 rounded-full tracking-widest text-xs'> DIGITAL BANKING PLATFORM
+      <button className='bg-gray-100 px8 py-2 rounded-full tracking-widest text-xs'> DIGITAL BANKING PLATFORM
       </button>
     </div>
   )
