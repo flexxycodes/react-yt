c05d5ce008cbd19f4797a71699974cc34b194655
flexxycodes <flexxycodes@gmail.com>
2026-01-02 18:14:52 +0530

    update App.jsx

--- 06-tailwind/src/App.jsx
+++ 06-tailwind/src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div className='h-screen w-full bg-gray-900'>
+    <div className='h-screen w-full bg-blue-50'>
       <h1> Heyyy!!</h1>
       
     </div>
