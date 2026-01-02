ac914f74d2082f49dfb5cc7c524490b91cf22f67
flexxycodes <flexxycodes@gmail.com>
2026-01-02 20:20:53 +0530

    update

--- 06-tailwind/src/components/Section1/Navbar.jsx
+++ 06-tailwind/src/components/Section1/Navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Navbar = () => {
   return(
-    <div className='flex items-center justify-between px-12 py-6'>
+    <div className='flex items-center justify-between px-18 py-8'>
       <h4 className='bg-black text-white px-6 py-2 rounded-full'>TARGET AUDIENCE</h4>
       <button className='bg-gray-100 px8 py-2 rounded-full tracking-widest text-xs'> DIGITAL BANKING PLATFORM
       </button>
---
+++ 06-tailwind/src/components/Section1/Page1Content.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Page1Content = () => {
+  return(
+    <div className='py-3 bg-blue-500 px-18'>
+      
+    </div>
+  )
+};
+
+export default Page1Content;
