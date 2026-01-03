8c61c067b75e801e2f7a77064140f8e6457b91b2
flexxycodes <flexxycodes@gmail.com>
2026-01-03 19:41:07 +0530

    Add Sections

--- 06-tailwind/src/components/Section1/Section1.jsx
+++ 06-tailwind/src/components/Section1/Section1.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
 import Navbar from './Navbar'
-import Page1Content from './Page1Content'
 
-const Section1 = (props) => {
+const Section1 = () => {
   return (
-    <div className='h-screen w-full '>
+    <div className='h-screen w-full bg-blue-200'>
         <Navbar />
     </div>
   )
