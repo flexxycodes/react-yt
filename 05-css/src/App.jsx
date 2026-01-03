cc2b74d68d28cd99629690b3a43d4c63159acdcb
flexxycodes <flexxycodes@gmail.com>
2026-01-03 20:51:48 +0530

    update

--- 05-css/src/App.jsx
+++ 05-css/src/App.jsx
@@ -1,12 +1,8 @@
 import React from 'react'
-import Button from './components/button/Button'
-import Header from './components/header/Header'
 
 const App = () => {
   return (
     <div>
-      <Button />
-      <Header />
     </div>
   )
 }
--- 06-tailwind/src/components/Stats.jsx
+++ 06-tailwind/src/components/Stats.jsx
@@ -3,7 +3,7 @@ export default function Stats() {
     <section className="flex flex-col items-center text-center py-16 gap-4">
       <h2 className="text-3xl font-bold">A growing library of</h2>
 
-      <div className="text-4xl font-extrabold">
+      <div className="text-4xl font-extrabold leading-">
         <p>1,150 apps</p>
         <p>570,300 screens</p>
         <p>302,500 flows</p>
