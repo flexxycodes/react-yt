5551defd15561dcbbe5835a98dd250843f3ef060
flexxycodes <flexxycodes@gmail.com>
2026-01-03 20:16:04 +0530

    update Navbar.jsx

--- 06-tailwind/src/components/Section1/Navbar.jsx
+++ 06-tailwind/src/components/Section1/Navbar.jsx
@@ -2,9 +2,7 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div className='flex items-center justify-between py-4 px-9'>
-        <h4 className='bg-black text-white uppercase px-6 py-2 rounded-full'>Target Audience</h4>
-        <button className='bg-gray-200 px-6 py-2 uppercase rounded-full tracking-widest text-sm'>digital Banking Platform</button>
+    <div className='fixed top-0 left-0 right-0 flex items-center justify-between py-2 px-4 rounded-full bg-gray 100'>
     </div>
   )
 }
