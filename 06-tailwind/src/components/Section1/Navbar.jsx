fc79bbd36e2f4c003a2cdc2988dc1424517df9f3
flexxycodes <flexxycodes@gmail.com>
2026-01-03 20:21:11 +0530

    update Navbar.jsx

--- 06-tailwind/src/components/Section1/Navbar.jsx
+++ 06-tailwind/src/components/Section1/Navbar.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 const Navbar = () => {
   return (
     <div className='fixed flex items-center justify-between py-2 px-4 rounded-full bg-gray 100 w-full h-20px'>
+      hi
     </div>
   )
 }
