adbfb21933f241e25cb022e0a5ee83545515c487
flexxycodes <flexxycodes@gmail.com>
2026-01-03 21:10:08 +0530

    update Navbar.jsx

--- 05-css/src/components/Navbar.jsx
+++ 05-css/src/components/Navbar.jsx
@@ -1,17 +1,18 @@
-export const Navbar = () => {
+export default function Navbar() {
   return (
-    <div className='fixed top-4 left-0 right-0 z-50 flex justify-center'>
-      <nav className='w-[92%] max-w-5xl flex items-center justify-between shadow-sm backdrop-blur px-6 py-3 rounded-full bg-[#F4F4F4]'>
+    <div className="fixed top-4 left-0 right-0 z-50 flex justify-center">
+      <nav className="w-[92%] max-w-5xl flex items-center justify-between px-6 py-3 
+                      bg-[#F4F4F4] rounded-full shadow-sm backdrop-blur">
         
-        <div className='text-xl font-semibold'></div>
-        
-        <button className='flex flex-col gap-1.5'>
-          <span clasName='w-4 h-0.5 bg-black'></span>
-          <span clasName='w-3.5 h-0.5 bg-black'></span>
+        {/* Logo */}
+        <div className="text-xl font-bold">M</div>
+
+        {/* Menu icon */}
+        <button className="flex flex-col gap-1.5">
+          <span className="w-5 h-0.5 bg-black"></span>
+          <span className="w-5 h-0.5 bg-black"></span>
         </button>
       </nav>
     </div>
-  )
+  );
 }
-
-export default Navbar
